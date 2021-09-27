e6349ed27ea97b84d300d3578c300e86f59b5b24
Anton-Polkanov <anton.polkanov@prooftec.com>
2021-09-27 21:38:53 +1000

    update Setup script

--- load_scripts/Setup_v2.sql
+++ load_scripts/Setup_v2.sql
@@ -27,58 +27,53 @@ GO
 
 
 -- Create Tables
-
 CREATE TABLE DimCustomer (
-	Customer_Key int identity not null,					-- Surrogate key for Dimension Table
-	CustomerID nvarchar(3) not null,					-- customer natural key
+	Customer_ID nvarchar(50) not null,					-- customer natural key
 	Customer_First_Name nvarchar(20) null,
 	Customer_Surname nvarchar(20) null,					-- note not null may not always be true	
-	Primary Key (Customer_Key)
+	Primary Key (Customer_ID)
 )
 
 CREATE TABLE DimStaff (
-	Staff_Key int identity not null,				-- Surrogate key for Dimension Table	
-	Staff_ID nvarchar(5) null,						-- staff natural key identifer
+	Staff_ID nvarchar(50) not null,						-- staff natural key identifer
 	Staff_First_Name	nvarchar(20) null,				
 	Staff_Surname		nvarchar(20) null,
 	Location_ID nvarchar(2) not null,
 	Location_Name nvarchar(20) not null,
-	Primary Key (Staff_Key),
+	Primary Key (Staff_ID),
 )
 
 CREATE TABLE DimItem (					
-	Item_Key int identity not null,					--Surrogate dimension table key
-	Item_ID	nvarchar(5)	null,					-- Natural Tem key
+	Item_ID	nvarchar(50)	not null,					-- Natural Tem key
 	Item_Description nvarchar(30) null,
 	Item_Unit_Price float null,
-	Primary Key (Item_Key)
+	Primary Key (Item_ID)
 )
 
 CREATE TABLE DimDate (
-	Date_Key int identity not null,
-	Date_date date null,
-	Date_day int null,			
-	Date_Month int	null,					
-	Date_Quarter int null,
-	Date_Year int null,
-	Primary Key (Date_Key)
+	Sale_Date datetime2 not null,
+	Day int null,			
+	Month int	null,					
+	Quarter int null,
+	Year int null,
+	Primary Key (Sale_Date)
 )
 
 
 CREATE TABLE FactSales (
-	Date_Key int not null,
-	Customer_Key int not null,
-	Staff_Key int not null,
-	Item_Key int not null,
-	Receipt_ID nvarchar(5) not null,
-	Receipt_Transaction_Row_ID nvarchar(5) not null,
+	Sale_Date datetime2 not null,
+	Customer_ID nvarchar(50) not null,
+	Staff_ID nvarchar(50) not null,
+	Item_ID nvarchar(50) not null,
+	Receipt_ID nvarchar(50) not null,
+	Receipt_Transaction_Row_ID nvarchar(50) not null,
 	Item_Quantity int not null,	
 	Row_Total float null,
-	PRIMARY KEY (Date_Key, Customer_Key, Staff_Key, Item_Key, Receipt_ID, Receipt_Transaction_Row_ID),
-	FOREIGN KEY (Date_Key) REFERENCES DimDate (Date_Key),
-	FOREIGN KEY (Customer_Key) REFERENCES DimCustomer (Customer_Key),
-	FOREIGN KEY (Staff_Key) REFERENCES DimStaff (Staff_Key),
-	FOREIGN KEY (Item_Key) REFERENCES DimItem (Item_Key),
+	PRIMARY KEY (Sale_Date, Customer_ID, Staff_ID, Item_ID, Receipt_ID, Receipt_Transaction_Row_ID),
+	FOREIGN KEY (Sale_Date) REFERENCES DimDate (Sale_Date),
+	FOREIGN KEY (Customer_ID) REFERENCES DimCustomer (Customer_ID),
+	FOREIGN KEY (Staff_ID) REFERENCES DimStaff (Staff_ID),
+	FOREIGN KEY (Item_ID) REFERENCES DimItem (Item_ID),
 )
 go
 
