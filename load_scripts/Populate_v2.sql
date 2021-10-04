9ee8b5a431045375629ce45069834f5b9263ed4d
Anton-Polkanov <anton.polkanov@prooftec.com>
2021-10-04 22:54:19 +1100

    replace CTE with a temporary table

--- load_scripts/Populate_v2.sql
+++ load_scripts/Populate_v2.sql
@@ -7,65 +7,102 @@
 USE Assignment1_v2
 GO
 
--- Delete null data 
-DELETE FROM SalesData WHERE "Customer_ID" IS NULL;
+-- Delete the last empty row added by SSIS import
+DELETE FROM SalesData WHERE "Customer ID" IS NULL;
 
+IF OBJECT_ID('tempdb..#tmp') IS NOT NULL
+BEGIN
+	DROP TABLE #tmp
+END
+GO
 
---Remove duplicate rows
-WITH CTE AS(
-   SELECT "Sale_Date", "Reciept_Id", "Customer_ID", "Customer_First_Name", "Customer_Surname", "Staff_ID", "Staff_First_Name", "Staff_Surname", "Staff_office", "Office_Location", "Reciept_Transaction_Row_ID", "Item_ID", "Item_Description", "Item_Quantity", "Item_Price", "Row_Total",
-       RN = ROW_NUMBER() OVER(PARTITION BY "Sale_Date", "Reciept_Id", "Customer_ID", "Customer_First_Name", "Customer_Surname", "Staff_ID", "Staff_First_Name", "Staff_Surname", "Staff_office", "Office_Location", "Reciept_Transaction_Row_ID", "Item_ID", "Item_Description", "Item_Quantity", "Item_Price", "Row_Total" ORDER BY "Sale_Date")
-   FROM SalesData
-)
-DELETE FROM CTE WHERE RN > 1
+SELECT 
+		"Sale Date" AS Sale_Date, 
+		"Reciept Id" AS Receipt_ID,
+		"Customer ID" AS Customer_ID,
+		"Customer First Name" AS Customer_First_Name,
+		"Customer Surname" AS Customer_Surname,
+		"Staff ID" AS Staff_ID,
+		"Staff First Name" AS Staff_First_Name,
+		"Staff Surname" AS Staff_Surname,
+		"Staff office" AS Staff_Office,
+		"Office Location" AS Office_Location,
+		"Reciept Transaction Row ID" AS Receipt_Transaction_Row_ID,
+		"Item ID" AS Item_ID,
+		"Item Description" AS Item_Description,
+		"Item Quantity" AS Item_Quantity,
+		"Item Price" AS Item_Price,
+		"Row Total" AS Row_Total,
+    RN = ROW_NUMBER() OVER(PARTITION BY 
+				"Sale Date",
+				"Reciept Id",
+				"Customer ID",
+				"Customer First Name",
+				"Customer Surname",
+				"Staff ID",
+				"Staff First Name",
+				"Staff Surname",
+				"Staff office",
+				"Office Location",
+				"Reciept Transaction Row ID",
+				"Item ID",
+				"Item Description",
+				"Item Quantity",
+				"Item Price",
+				"Row Total"
+				ORDER BY "Sale Date")
+INTO #tmp
+FROM SalesData
 
+-- Remove duplicate rows
+DELETE FROM #tmp WHERE RN > 1
 
 -- Create Customer Dimension
-Insert into DimCustomer
+INSERT INTO DimCustomer
 (Customer_ID, Customer_First_Name, Customer_Surname)
-SELECT DISTINCT "Customer_ID", "Customer_First_Name", "Customer_Surname" From CTE ORDER BY "Customer_ID";
+SELECT DISTINCT Customer_ID, Customer_First_Name, Customer_Surname FROM #tmp ORDER BY Customer_ID;
 
 
 -- Create Staff Dimension
-Insert into DimStaff
+INSERT INTO DimStaff
  (Staff_ID, Staff_First_Name, Staff_Surname, Location_ID, Location_Name)
-SELECT DISTINCT "Staff_ID", "Staff_First_Name", "Staff_Surname", "Staff_office", "Office_Location"
-FROM CTE ORDER BY "Staff_ID"
+SELECT DISTINCT Staff_ID, Staff_First_Name, Staff_Surname, Staff_office, Office_Location
+FROM #tmp ORDER BY Staff_ID
 
 
 INSERT INTO DimDate 
 (Sale_Date, Day, Month, Quarter, Year)
-SELECT DISTINCT SalesData.[Sale_Date], DAY(SalesData.[Sale_Date]), MONTH(SalesData.[Sale_Date]), DATEPART(quarter, SalesData.[Sale_Date]), YEAR(SalesData.[Sale_Date]) 
-FROM CTE ORDER BY "Sale_Date";
+SELECT DISTINCT Sale_Date, DAY(Sale_Date), MONTH(Sale_Date), DATEPART(quarter, Sale_Date), YEAR(Sale_Date) 
+FROM #tmp ORDER BY Sale_Date;
 
 
 INSERT INTO DimItem 
 (Item_ID, Item_Description, Item_Unit_Price)
-SELECT DISTINCT SalesData.[Item_ID], SalesData.[Item_Description], SalesData.[Item_Price]
-FROM CTE WHERE "Item_Description" is not NULL ORDER BY "Item_ID";
+SELECT DISTINCT Item_ID, Item_Description, Item_Price
+FROM #tmp WHERE Item_Description IS NOT NULL ORDER BY Item_ID;
 
 
 --Now Populate the Fact Table
-Insert into FactSales
+INSERT INTO FactSales
 (Dim_Date_ID, Dim_Customer_ID, Dim_Staff_ID, Dim_Item_ID, Receipt_ID, Receipt_Transaction_Row_ID, Item_Quantity, Row_Total)
 SELECT  DISTINCT DimDate.[ID] AS Dim_Date_ID, 
 DimCustomer.[ID] AS Dim_Customer_ID, 
 DimStaff.[ID] AS Dim_Staff_ID, 
 DimItem.[ID] AS Dim_Item_ID, 
-CTE.[Reciept_Id],
-CTE.[Reciept_Transaction_Row_ID],
-CTE.[Item_Quantity],
-CTE.[Row_Total]
-FROM CTE 
-	left join DimCustomer
-		on CTE.[Customer_ID]=DimCustomer.Customer_ID
-	left Join DimDate
-		on CTE.[Sale_Date]=DimDate.Sale_Date
-	left join DimItem
-		on CTE.[Item_ID]=DimItem.Item_ID
-	Left join DimStaff
-		on CTE.[Staff_ID]=DimStaff.Staff_ID
-ORDER BY "Reciept_Id"
+#tmp.[Receipt_ID],
+#tmp.[Receipt_Transaction_Row_ID],
+#tmp.[Item_Quantity],
+#tmp.[Row_Total]
+FROM #tmp 
+	JOIN DimCustomer
+		ON #tmp.[Customer_ID]=DimCustomer.Customer_ID
+	JOIN DimDate
+		ON #tmp.[Sale_Date]=DimDate.Sale_Date
+	JOIN DimItem
+		ON #tmp.[Item_ID]=DimItem.Item_ID
+	JOIN DimStaff
+		ON #tmp.[Staff_ID]=DimStaff.Staff_ID
+ORDER BY Receipt_ID
 
 
 SELECT * FROM FactSales;
