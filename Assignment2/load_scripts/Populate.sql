b249da554fe02be54dffb164aefd7b8b6a7620dd
Anton-Polkanov <anton.polkanov@prooftec.com>
2021-10-20 20:26:26 +1100

    Ass2: update loading scripts

---
+++ Assignment2/load_scripts/Populate.sql
@@ -0,0 +1,110 @@
+/*************************************************
+ *				Assignment 2		             *
+ *				Team: 7							 *
+ *************************************************/
+
+USE Assignment2
+GO
+
+-- Delete the last empty row added by the Import Data task
+DELETE FROM SalesData WHERE "Customer ID" IS NULL;
+
+IF OBJECT_ID('tempdb..#tmp') IS NOT NULL
+BEGIN
+	DROP TABLE #tmp
+END
+GO
+
+SELECT 
+		"Sale Date" AS Sale_Date, 
+		"Reciept Id" AS Receipt_ID,
+		"Customer ID" AS Customer_ID,
+		"Customer First Name" AS Customer_First_Name,
+		"Customer Surname" AS Customer_Surname,
+		"Staff ID" AS Staff_ID,
+		"Staff First Name" AS Staff_First_Name,
+		"Staff Surname" AS Staff_Surname,
+		"Staff office" AS Location_ID,
+		"Office Location" AS Location_Name,
+		"Reciept Transaction Row ID" AS Receipt_Transaction_Row_ID,
+		"Item ID" AS Item_ID,
+		"Item Description" AS Item_Description,
+		"Item Quantity" AS Item_Quantity,
+		"Item Price" AS Item_Price,
+		"Row Total" AS Row_Total,
+    RN = ROW_NUMBER() OVER(PARTITION BY 
+				"Sale Date",
+				"Reciept Id",
+				"Customer ID",
+				"Customer First Name",
+				"Customer Surname",
+				"Staff ID",
+				"Staff First Name",
+				"Staff Surname",
+				"Staff office",
+				"Office Location",
+				"Reciept Transaction Row ID",
+				"Item ID",
+				"Item Description",
+				"Item Quantity",
+				"Item Price",
+				"Row Total"
+				ORDER BY "Sale Date")
+INTO #tmp
+FROM SalesData
+
+-- Remove duplicate rows
+DELETE FROM #tmp WHERE RN > 1
+
+-- Create Customer Dimension
+INSERT INTO DimCustomer
+(Customer_ID, Customer_First_Name, Customer_Surname)
+SELECT DISTINCT Customer_ID, Customer_First_Name, Customer_Surname FROM #tmp ORDER BY Customer_ID;
+
+
+-- Create Staff Dimension
+INSERT INTO DimStaff
+ (Staff_ID, Staff_First_Name, Staff_Surname, Location_ID, Location_Name)
+SELECT DISTINCT Staff_ID, Staff_First_Name, Staff_Surname, Location_ID, Location_Name
+FROM #tmp ORDER BY Staff_ID
+
+
+INSERT INTO DimDate 
+(Sale_Date, Day, Month, Quarter, Year)
+SELECT DISTINCT Sale_Date, DAY(Sale_Date), MONTH(Sale_Date), DATEPART(quarter, Sale_Date), YEAR(Sale_Date) 
+FROM #tmp ORDER BY Sale_Date;
+
+
+INSERT INTO DimItem 
+(Item_ID, Item_Description, Item_Unit_Price)
+SELECT DISTINCT Item_ID, Item_Description, Item_Price
+FROM #tmp WHERE Item_Description IS NOT NULL ORDER BY Item_ID;
+
+
+--Now Populate the Fact Table
+INSERT INTO FactSales
+(Dim_Date_ID, Dim_Customer_ID, Dim_Staff_ID, Dim_Item_ID, Receipt_ID, Receipt_Transaction_Row_ID, Location_ID, Item_Quantity, Row_Total)
+SELECT  DISTINCT DimDate.[ID] AS Dim_Date_ID, 
+DimCustomer.[ID] AS Dim_Customer_ID, 
+DimStaff.[ID] AS Dim_Staff_ID, 
+DimItem.[ID] AS Dim_Item_ID, 
+#tmp.[Receipt_ID],
+#tmp.[Receipt_Transaction_Row_ID],
+#tmp.[Location_ID],
+#tmp.[Item_Quantity],
+#tmp.[Row_Total]
+FROM #tmp 
+	JOIN DimCustomer
+		ON #tmp.[Customer_ID]=DimCustomer.Customer_ID
+	JOIN DimDate
+		ON #tmp.[Sale_Date]=DimDate.Sale_Date
+	JOIN DimItem
+		ON #tmp.[Item_ID]=DimItem.Item_ID
+	JOIN DimStaff
+		ON #tmp.[Staff_ID]=DimStaff.Staff_ID
+ORDER BY Receipt_ID
+
+
+SELECT COUNT(*) FROM FactSales;
+
+
---
+++ Assignment2/load_scripts/Setup.sql
@@ -0,0 +1,85 @@
+/*************************************************
+ *				Assignment 2		             *
+ *				Team: 7							 *
+ *************************************************/
+
+USE MASTER
+GO
+IF DB_ID (N'Assignment2') IS NOT NULL -- Check if it already exists and drop if it is
+DROP DATABASE Assignment2;
+GO
+CREATE DATABASE Assignment2;
+GO
+
+USE Assignment2
+go
+
+
+ -- Drop tables if they exist
+DROP TABLE IF EXISTS FactSales;	
+DROP TABLE IF EXISTS DimCustomer;
+DROP TABLE IF EXISTS DimStaff;
+DROP TABLE IF EXISTS DimDate;
+DROP TABLE IF EXISTS DimItem;
+
+GO
+
+
+-- Create Tables
+CREATE TABLE DimCustomer (
+	ID uniqueidentifier DEFAULT NEWSEQUENTIALID(),
+	Customer_ID nvarchar(50) not null,					-- customer natural key
+	Customer_First_Name nvarchar(20) null,
+	Customer_Surname nvarchar(20) null,					-- note not null may not always be true	
+	Primary Key (ID)
+)
+
+CREATE TABLE DimStaff (
+	ID uniqueidentifier DEFAULT NEWSEQUENTIALID(),
+	Staff_ID nvarchar(50) not null,						-- staff natural key identifer
+	Staff_First_Name	nvarchar(20) null,				
+	Staff_Surname		nvarchar(20) null,
+	Location_ID int not null,
+	Location_Name nvarchar(20) not null,
+	Primary Key (ID),
+)
+
+CREATE TABLE DimItem (		
+	ID uniqueidentifier DEFAULT NEWSEQUENTIALID(),			
+	Item_ID	nvarchar(50)	not null,					-- Natural Tem key
+	Item_Description nvarchar(30) null,
+	Item_Unit_Price float null,
+	Primary Key (ID)
+)
+
+CREATE TABLE DimDate (
+	ID uniqueidentifier DEFAULT NEWSEQUENTIALID(),
+	Sale_Date datetime2 not null,
+	Day int null,			
+	Month int	null,					
+	Quarter int null,
+	Year int null,
+	Primary Key (ID)
+)
+
+
+CREATE TABLE FactSales (
+	Dim_Date_ID uniqueidentifier not null,
+	Dim_Customer_ID uniqueidentifier not null,
+	Dim_Staff_ID uniqueidentifier not null,
+	Dim_Item_ID uniqueidentifier not null,
+	Receipt_ID nvarchar(50) not null,
+	Receipt_Transaction_Row_ID nvarchar(50) not null,
+	Location_ID int not null,
+	Item_Quantity int not null,	
+	Row_Total float null,
+	PRIMARY KEY (Dim_Date_ID, Dim_Customer_ID, Dim_Staff_ID, Dim_Item_ID, Receipt_ID, Receipt_Transaction_Row_ID, Location_ID),
+	FOREIGN KEY (Dim_Date_ID) REFERENCES DimDate (ID),
+	FOREIGN KEY (Dim_Customer_ID) REFERENCES DimCustomer (ID),
+	FOREIGN KEY (Dim_Staff_ID) REFERENCES DimStaff (ID),
+	FOREIGN KEY (Dim_Item_ID) REFERENCES DimItem (ID)
+)
+go
+
+
+
