225517faa8cbf4abdb437d1f66ae6ced80e3bcce
Anton-Polkanov <anton.polkanov@prooftec.com>
2021-11-01 18:25:01 +1100

    add queries calculated based on data with discounted prices

--- Assignment2/query_scripts/Calculate_discounted_prices.sql
+++ Assignment2/query_scripts/Calculate_discounted_prices.sql
@@ -45,18 +45,67 @@
 		FROM AggregatedOrders
 	)
 	
-SELECT DATEFROMPARTS(dd.Year, dd.Month, dd.Day) as [Date]
-	,dc.Customer_First_Name + ' ' + dc.Customer_Surname as [Customer_Name]
-	,ds.Staff_First_Name + ' ' + ds.Staff_Surname as [Staff_Name]
-	,di.Item_Description as [Item_Description]
-	,[Receipt_Id]
-	,[Receipt_Transaction_Row_ID]
-	,ds.[Location_Name] as [Location_Name]
-	,[Item_Quantity]
-	,[Row_Total]
-	,[Discounted_Row_Total]
-FROM OrdersWithDiscounts owd 
-	JOIN DimDate dd ON owd.Dim_Date_ID = dd.ID
-	JOIN DimCustomer dc ON owd.Dim_Customer_ID = dc.ID
-	JOIN DimStaff ds ON owd.Dim_Staff_ID = ds.ID
-	JOIN DimItem di ON owd.Dim_Item_ID = di.ID
+	,[FactSalesWithDiscountedPrices] AS (
+	SELECT DATEFROMPARTS(dd.Year, dd.Month, dd.Day) as [Date]
+		,dc.Customer_First_Name + ' ' + dc.Customer_Surname as [Customer_Name]
+		,ds.Staff_First_Name + ' ' + ds.Staff_Surname as [Staff_Name]
+		,di.Item_Description as [Item_Description]
+		,[Receipt_Id]
+		,[Receipt_Transaction_Row_ID]
+		,ds.[Location_Name] as [Location_Name]
+		,[Item_Quantity]
+		,[Row_Total]
+		,[Discounted_Row_Total]
+	FROM OrdersWithDiscounts owd 
+		JOIN DimDate dd ON owd.Dim_Date_ID = dd.ID
+		JOIN DimCustomer dc ON owd.Dim_Customer_ID = dc.ID
+		JOIN DimStaff ds ON owd.Dim_Staff_ID = ds.ID
+		JOIN DimItem di ON owd.Dim_Item_ID = di.ID
+	)
+
+-- 1. Transactions with the maximum and minimum number of items sold 
+--SELECT 
+--	Location_Name
+--	,MAX(Items_In_Order) as Max_Items_In_Order
+--	,MIN(Items_In_Order) as Min_Items_In_Order
+--FROM (
+--	SELECT SUM(Item_Quantity) as Items_In_Order, Location_Name
+--	FROM [FactSalesWithDiscountedPrices]
+--	GROUP BY Receipt_ID, Location_Name
+--) as tmp
+--GROUP BY Location_Name
+--ORDER BY Max_Items_In_Order DESC
+
+-- 2. Average bucket size
+--SELECT 
+--	Location_Name
+--	,AVG(Items_In_Order) as Avg_Items_In_Order
+--FROM (
+--	SELECT SUM(Item_Quantity) as Items_In_Order, Location_Name
+--	FROM [FactSalesWithDiscountedPrices]
+--	GROUP BY Receipt_ID, Location_Name
+--) as tmp
+--GROUP BY Location_Name
+--ORDER BY Avg_Items_In_Order DESC
+
+-- 3. Average ticket size
+--SELECT 
+--	Location_Name
+--	,AVG(Sum_Of_Order) as Avg_Sum_Of_Order
+--FROM (
+--	SELECT SUM(Discounted_Row_Total) as Sum_Of_Order, Location_Name
+--	FROM [FactSalesWithDiscountedPrices]
+--	GROUP BY Receipt_ID, Location_Name
+--) as tmp
+--GROUP BY Location_Name
+--ORDER BY Avg_Sum_Of_Order DESC
+
+-- 4. Finds 3 best/worst performing items in each store
+SELECT Location_Name, Item_Description, SUM(Item_Quantity) as Sum_Of_Items
+FROM [FactSalesWithDiscountedPrices]
+GROUP BY Location_Name, Item_Description
+ORDER BY Location_Name ASC, Sum_Of_Items DESC
+
+
+
+
